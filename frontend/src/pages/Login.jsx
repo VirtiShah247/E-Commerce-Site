23aa467673ea3a63e8689ba9892775ed3d0fc0a7
Virti Shah <vpshah247@gmail.com>
2024-03-16 13:00:26 +0530

    remove unwanted commits

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,9 @@
 import LoginRegisterComponent from "../components/LoginRegisterComponent"
 
-// import React from 'react'
 const Login = () => {
-    const title = "Welcome Back. Please Log In To Your Account.";
-    const componentName = "Login";
     return (
         <>
-           <LoginRegisterComponent title = {title} componentName = {componentName}/>
-            {/* <LoginRegisterComponent>
-                hello
-            </LoginRegisterComponent> */}
+           <LoginRegisterComponent title = "Welcome Back. Please Log In To Your Account." componentName = "Login"/>
         </>
     )
 }
