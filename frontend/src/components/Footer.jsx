523a8c0c411544946b6c6378cf255efebc7afabf
Virti Shah <vpshah247@gmail.com>
2024-04-06 22:43:07 +0530

    fix[footer]: make all heading uppercase and add gap between two containers

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,90 +7,90 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
     return (
         <Fragment>
-            <footer className="bg-dull-pink text-brown grid sm:grid-cols-2 justify-evenly place-items-center shadow-lg pt-[70px] pb-[40px] px-3">
+            <footer className="bg-dull-pink text-brown w-full grid sm:flex sm:flex-column sm:justify-evenly gap-20 sm:gap-5  place-items-center shadow-lg pt-[70px] pb-[40px] px-4">
 
-                <div className="grid grid-cols-3 gap-5 sm:gap-[80px] text-wrap text-start">
-                    <div>
-                        <h3 className="mb-2">Customer Service</h3>
-                        <ul>
-                            <Link to="/contact-us" className="hover:text-pink">
-                                <li>
-                                    Contact Us
-                                </li>
-                            </Link>
-                            <Link to="/delivery-returns" className="hover:text-pink">
-                                <li>
-                                    Delivery & Returns
-                                </li>
-                            </Link>
+                <div className="grid grid-cols-3 gap-3 sm:gap-10 text-wrap text-start">
+                        <div>
+                            <h4 className="mb-2 uppercase">Customer Service</h4>
+                            <ul>
+                                <Link to="/contact-us" className="hover:text-pink">
+                                    <li>
+                                        Contact Us
+                                    </li>
+                                </Link>
+                                <Link to="/delivery-returns" className="hover:text-pink">
+                                    <li>
+                                        Delivery & Returns
+                                    </li>
+                                </Link>
 
-                            {/* <li>
-                                Make An Appointment
-                            </li>
-                            <li>
-                                Bespoke Services
-                            </li> */}
-                        </ul>
-                    </div>
-                    <div>
-                        <h3 className="mb-2">Our Company</h3>
-                        <ul>
-                            <Link to="/about-us" className="hover:text-pink">
-                                <li>
-                                    About us
+                                {/* <li>
+                                    Make An Appointment
                                 </li>
-                            </Link>
-                            <Link to="/news" className="hover:text-pink">
                                 <li>
-                                    News
-                                </li>
-                            </Link>
-                            <Link to="/terms-and-conditions" className="hover:text-pink">
-                                <li>
-                                    Terms & Conditions
-                                </li>
-                            </Link>
-                            <Link to="/website-conditions" className="hover:text-pink">
-                                <li>
-                                    Website Conditions
-                                </li>
-                            </Link>
-                            <Link to="privacy-policy" className="hover:text-pink">
-                                <li>
-                                    Privacy Policy
-                                </li>
-                            </Link>
+                                    Bespoke Services
+                                </li> */}
+                            </ul>
+                        </div>
+                        <div>
+                            <h4 className="mb-2 uppercase">Our Company</h4>
+                            <ul>
+                                <Link to="/about-us" className="hover:text-pink">
+                                    <li>
+                                        About us
+                                    </li>
+                                </Link>
+                                <Link to="/news" className="hover:text-pink">
+                                    <li>
+                                        News
+                                    </li>
+                                </Link>
+                                <Link to="/terms-and-conditions" className="hover:text-pink">
+                                    <li>
+                                        Terms & Conditions
+                                    </li>
+                                </Link>
+                                <Link to="/website-conditions" className="hover:text-pink">
+                                    <li>
+                                        Website Conditions
+                                    </li>
+                                </Link>
+                                <Link to="privacy-policy" className="hover:text-pink">
+                                    <li>
+                                        Privacy Policy
+                                    </li>
+                                </Link>
 
-                            <Link to="/cookie-policy" className="hover:text-pink">
-                                <li>
-                                    Cookie Policy
-                                </li>
-                            </Link>
+                                <Link to="/cookie-policy" className="hover:text-pink">
+                                    <li>
+                                        Cookie Policy
+                                    </li>
+                                </Link>
 
 
-                        </ul>
-                    </div>
-                    <div>
-                        <h3 className="mb-2">Also Of Interest</h3>
-                        <ul>
-                            <Link to="/men" className="hover:text-pink">
-                                <div>Men</div>
-                            </Link>
-                            <Link to="/women" className="hover:text-pink">
-                                <div>Women</div>
-                            </Link>
-                            <Link to="/kids" className="hover:text-pink">
-                                <div>Kids</div>
-                            </Link>
-                            <Link to="/beauty" className="hover:text-pink">
-                                <div>Beauty</div>
-                            </Link>
-                        </ul>
-                    </div>
+                            </ul>
+                        </div>
+                        <div>
+                            <h4 className="mb-2 uppercase">Also Of Interest</h4>
+                            <ul>
+                                <Link to="/men" className="hover:text-pink">
+                                    <div>Men</div>
+                                </Link>
+                                <Link to="/women" className="hover:text-pink">
+                                    <div>Women</div>
+                                </Link>
+                                <Link to="/kids" className="hover:text-pink">
+                                    <div>Kids</div>
+                                </Link>
+                                <Link to="/beauty" className="hover:text-pink">
+                                    <div>Beauty</div>
+                                </Link>
+                            </ul>
+                        </div>
                 </div>
                 <div className="text-center grid gap-y-4 -order-1 sm:order-1">
                     <div className="grid gap-y-2">
-                        <h3>Stay in touch</h3>
+                        <h4 className="uppercase">Stay in touch</h4>
                         <p>Be the first to hear about new collections and exclusive events.</p>
                     </div>
                     <form className="flex flex-col items-center relative ps-2 pe-3">
