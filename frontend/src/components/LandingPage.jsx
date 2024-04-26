38ad326f5d8ca5abbccc647d8e272bc4e957281a
Virti Shah <vpshah247@gmail.com>
2024-04-26 20:43:06 +0530

    add react-slick library in landing page

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,11 +1,10 @@
-import { Fragment, useEffect, useRef, useState } from "react"
+import { Fragment } from "react"
 import landingPage1 from "../assets/landing-page-1.jpg";
 import landingPage2 from "../assets/landing-page-2.jpg";
-// import landingPage3 from "../assets/wepik-export.png";
-// import { Link } from "react-router-dom";
-// import { AiFillFacebook, AiFillYoutube, AiOutlineInstagram } from "react-icons/ai";
-// import { FaPinterest } from "react-icons/fa";
-// import { RiTwitterXLine } from "react-icons/ri";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 export const LandingPage = () => {
     const landingPages = [{
         id: 1,
@@ -15,95 +14,32 @@ export const LandingPage = () => {
         id: 2,
         imageLink: landingPage2
     },
-    // {
-    //     id: 3,
-    //     imageLink: landingPage2
-    // },
     ];
-    let sliderInterval;
-    const [landingPageNo, setLandingPageNo] = useState(0);
-    const sliderRef = useRef();
-    // const removeAnimation = () => {
-    //     sliderRef.current.classList.remove("fade-animation");
-    // }
-    useEffect(() => {
-        
-        startSlider();
-        return () => {
-            pauseSlider();
-        }
-    }, []);
-    const startSlider = () => {
-        sliderInterval = setInterval(() => {
-            handleNextSlide();
-        }, 2000);
-    }
-    const pauseSlider = () => {
-        clearInterval(sliderInterval);
-    }
-    const handleNextSlide = () => {
-
-        setLandingPageNo(landingPageNo => (landingPageNo + 1) % landingPages.length);
-        // sliderRef.current.classList.add("fade-animation");
-        // sliderRef.current.addEventListener("animationend", removeAnimation);
-        sliderRef.current.addEventListener("mouseenter", pauseSlider);
-        sliderRef.current.addEventListener("mouseleave", startSlider);
-        console.log("ref: " + sliderRef.current);
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
 
     }
     return (
         <Fragment>
-            <div className="relative">
-                {
-                    landingPages.map(landingPage =>
-                        <div key={landingPage.id} className={`${landingPage.id == landingPageNo + 1 ? "grid" : "hidden"} "w-full text-black-500"`}>
-                            {/* <div className="w-1/2 absolute grid gap-4 px-5 fade-animation">
-                                <h2>Lorem Ipsum</h2>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci autem quam laborum inventore ut ex amet incidunt eum culpa exercitationem, quos itaque dolores placeat hic ullam repudiandae optio cumque! Exercitationem.</p>
-                                <button className="bg-dark-yellow py-2">Learn more</button>
-                                <div className="flex gap-2">
-                                    <Link to="https://www.instagram.com/" className="hover:opacity-50">
-                                        <div>
-                                            <AiOutlineInstagram size={25} />
-                                        </div>
-                                    </Link>
-                                    <Link to="https://www.facebook.com/" className="hover:opacity-50">
-                                        <div>
-                                            <AiFillFacebook size={25} />
-                                        </div>
-                                    </Link>
-                                    <Link to="https://in.pinterest.com/" className="hover:opacity-50">
-                                        <div>
-                                            <FaPinterest size={25} />
-                                        </div>
-                                    </Link>
-                                    <Link to="https://twitter.com/" className="hover:opacity-50">
-                                        <div>
-                                            <RiTwitterXLine size={25} />
-                                        </div>
-                                    </Link>
-                                    <Link to="https://youtube.com/" className="hover:opacity-50">
-                                        <div >
-                                            <AiFillYoutube size={25} />
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div> */}
-                            <div ref={sliderRef} className="">
-                                <img src={landingPage.imageLink} alt="landing-page" className="w-full h-auto max-h-[600px] fade-animation" />
-                            </div>
-                        </div>)
-                }
-                <form action="" className="absolute bottom-0 left-1/2 space-x-3">
+            <div className="max-w-fit min-w-[200px]">
+                <Slider {...settings} className="relative max-w-fit min-w-[200px]">
                     {
-                        landingPages.map(landingPage => <button type="button" id={`radio${landingPage.id}`} key={landingPage.id} onClick={() => setLandingPageNo(landingPage.id - 1)}
-                            className={`w-3 h-3 bg-off-white rounded-full  hover:bg-dark-yellow  cursor-pointe focus:ring-1 focus:brown   ${landingPage.id == landingPageNo + 1 && "!bg-brown"}`}
-                        />)
+                        landingPages.map(landingPage =>
+                            <div key={landingPage.id} className="w-full">
+                                <div>
+                                    <img src={landingPage.imageLink} alt="landing-page" className="w-full h-auto max-h-[600px]" />
+                                </div>
+                            </div>)
                     }
-                </form>
-
+                </Slider>
             </div>
-
         </Fragment>
     )
 }
